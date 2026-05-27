06d3dead6e5c956c920e76e0b7b43937fd1052da
NaJungWon <128442467+NaJungWon@users.noreply.github.com>
2026-05-27 13:44:22 +0900

    26.05.27

---
+++ Assets/10. Scripts/1. Unity/08. Merge Game/AnimalBall.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+
+namespace Study.MergeGame
+{
+    public class AnimalBall : MonoBehaviour
+    {
+        public int level = 1;
+        //public bool hasFirstContact = false;
+
+        // C#의 프로퍼티를 이용해서 외부에서는 조회 가능하지만
+        // 변경에 대한 책임은 나만(private) 가질 수 있는
+        // 안전한 변수(사실은 필드)를 만들어줍니다
+        public bool HasFirstContact { get; private set; } = false;
+        public bool isMerged { get; set; } = false;
+
+        private Rigidbody2D rBody;
+        private Collider2D col;
+
+        private void Awake()
+        {
+            rBody = GetComponent<Rigidbody2D>();
+            col = GetComponent<Collider2D>();
+            col.enabled = false;
+            //  gravityScale은 중력의 영향을 얼마나 받는지 결정하는 변수입니다
+            rBody.gravityScale = 0.0f;
+            rBody.bodyType = RigidbodyType2D.Kinematic;
+        }
+
+        public void Drop()
+        {
+            col.enabled = true;
+            rBody.bodyType = RigidbodyType2D.Dynamic;
+            rBody.gravityScale = 1.0f;
+
+            // transform.SetParent()
+            // : 부모자식 관계를 형성해주는 함수 입니다. null 입력시
+            //  부모 transform이 없는 월드 최상위 개체가 됩니다
+            transform.SetParent(null);
+        }
+
+        // OnCollision 시리즈
+        // : OnTrigger와 함께 사용되는 물리 감지 이벤트 함수 입니다
+        // rigidbody를 통해서 감지 할 수 있습니다
+
+        // OnCollisionEnter2D
+        // : Trigger가 아닌 Collider와 충돌 했을때 호출되는 이벤트 함수입니다.
+
+        // OnCollisionStay2D
+        // : 물리적 충돌이 유지되고 있을때 호출되는 이벤트 함수입니다
+
+        // OnCollisionExit2D
+        // : 물리적 충돌이 종료 되었을때 호출되는 이벤트 함수입니다
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            HasFirstContact = true;
+
+            //두 볼이 병합되는 과정은
+            // 1. 접촉이 일어난다
+            // 2. 두 개체가 사라진다
+            // 3. 상우개체가 그 중앙에 생긴다
+
+            // 두 개체가 접촉해서 병합요청이 두번 호출 될 수 있으므로,
+            // 한쪽에 bool변수를 넣어서 중복호출 되지 않도록 해줍니다
+            if (isMerged) return;
+
+            // 충돌 정보에서 나와 충돌한 gameObject에서 AnimalBall 컴포넌트를
+            // 가져와 봅니다
+            AnimalBall contactBall = collision.gameObject.GetComponent<AnimalBall>();
+
+            // 벽에 부딪혀서 충돌한 개체에 AnimalBall 컴포넌트가 없다면
+            if (contactBall == null) return;
+
+            // 나의 레벨과 접촉한 볼의 level이 다르다면 return
+            if (level != contactBall.level) return;
+
+            MergeGameManager.Instance.MergeBall(this, contactBall);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(HasFirstContact && collision.CompareTag("Finish"))
+            {
+                MergeGameManager.Instance.GameOver();
+            }
+        }
+    }
+}
---
+++ Assets/10. Scripts/1. Unity/08. Merge Game/MergeGameManager.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
+
+namespace Study.MergeGame
+{
+    public class MergeGameManager : MonoBehaviour
+    {
+        // static 키워드는 랜드마크와 같습니다.
+        // 프로그램에서 정적(고정되어있다~)으로 선언되어있어서
+        // 전역적인 접근을 허용합니다.
+        public static MergeGameManager Instance;
+        public AnimalBall[] BallPrefabs;
+        public int maxIndexInQueue = 4;
+
+        private Queue<int> ballIndexQueue = new Queue<int>();
+
+        #region Unity Methods
+        private void Awake()
+        {
+            if (Instance == null) Instance = this;
+            else Destroy(gameObject);
+
+            maxIndexInQueue = Mathf.Min(maxIndexInQueue, BallPrefabs.Length);
+            maxIndexInQueue = Mathf.Max(maxIndexInQueue, 0);
+            ballIndexQueue.Enqueue(Random.Range(0, maxIndexInQueue));
+        }
+
+        #endregion
+
+        // 큐에서 꺼내온 녀석을 반환합니다. MergeGameController에서 호출합니다
+        public AnimalBall GetNextBall()
+        {
+            // 인덱스를 하나 뽑아주고
+            int dequeueIndex = ballIndexQueue.Dequeue();
+            // BallPrefabs의 길이만큼 랜덤한 숫자를 하나 뽑아서 큐에 담아줍니다
+            // 한번에 여러개 담는 처리해도 상관없음
+            ballIndexQueue.Enqueue(Random.Range(0, BallPrefabs.Length));
+
+            AnimalBall ball = GetBall(dequeueIndex);
+            return ball;
+        }
+
+        private AnimalBall GetBall(int index)
+        {
+            // 예외처리
+            if (index < 0 || BallPrefabs.Length <= index) return BallPrefabs[0];
+            return BallPrefabs[index];
+        }
+
+        // ballA 개체와 ballB 개체의 병합을 진행합니다
+        public void MergeBall(AnimalBall ballA, AnimalBall ballB)
+        {
+            // 두개체가 병합 진행상태임을 지정해줌
+            ballA.isMerged = true;
+            ballB.isMerged = true;
+
+            // 삭제하기 전에 중간 지점을 지역변수로 저장합니다.
+            Vector3 centerPosition = ballA.transform.position + ballB.transform.position;
+            centerPosition /= 2;
+
+            // 생성할 준비하기
+            int upgradeLevel = ballA.level;
+            // ball이 갖고 있는 level이 ballPrefab에서 다음 인덱스가 됨
+            // level 1 = index 0, level 2 = index 1;
+            // 상위 level 개체의 index는 ball의 level
+            AnimalBall upgradeBall = GetBall(upgradeLevel);
+
+            // ballA와 ballB를 삭제
+            ballA.gameObject.SetActive(false);
+            ballB.gameObject.SetActive(false);
+            // 두 개체를 비 활성화 해주는 이유는 삭제가 조금 느려서
+            // 충돌 안일어나게 하려고 일단 비활성화 시켜두고, 안전하게 삭제처리함
+
+            Destroy(ballA.gameObject);
+            Destroy(ballB.gameObject);
+
+            // 상위 볼 생성
+            AnimalBall spawnedBall = Instantiate(upgradeBall, centerPosition, Quaternion.identity);
+            spawnedBall.Drop();
+        }
+
+        // 게임오버가 되면 활성화 될 개체들과
+        [Header("GameOver Settings")]
+        public GameObject[] enableObjects;
+
+        // 게임오버가 되면 비활성화 될 개체들
+        public GameObject[] disableObjects;
+
+        public void GameOver()
+        {
+            for(int i = 0; i<enableObjects.Length;i++)
+            {
+                enableObjects[i].SetActive(true);
+            }
+
+            for (int i = 0; i < disableObjects.Length; i++)
+            {
+                disableObjects[i].SetActive(false);
+            }
+        }
+    }
+}
