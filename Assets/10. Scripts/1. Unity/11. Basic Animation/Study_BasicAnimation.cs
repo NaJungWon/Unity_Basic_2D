384aa0cbdc5fe02c9efc3cc1270ee9efef0ee2ee
NaJungWon <128442467+NaJungWon@users.noreply.github.com>
2026-06-10 13:49:47 +0900

    26.06.10 animation

---
+++ Assets/10. Scripts/1. Unity/11. Basic Animation/Study_BasicAnimation.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using Study.Utilities;
+using UnityEngine.InputSystem;
+
+namespace Study_BasicAnimation
+{
+    public class Study_BasicAnimation : MonoBehaviour
+    {
+        private Animator animator;
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            bool isWalk = SimpleInput.GetKey(Key.RightArrow) || SimpleInput.GetKey(Key.LeftArrow);
+            bool isRun = SimpleInput.GetKey(Key.LeftShift);
+
+            animator.SetBool("IsWalk", isWalk);
+            animator.SetBool("IsRun", isRun);
+        }
+    }
+}
